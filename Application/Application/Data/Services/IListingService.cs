a292dc59aaaa32bd86f23e065775bcd3076b88eb
Adam-I07 <90919913+Adam-I07@users.noreply.github.com>
2024-08-29 16:05:21 +0100

    Auction Application Initial Commit
    Database created - connection established and tables committed.
    Listing front end - code for both front end and back end for the listsing of auctions impelemented
    Adding items- form for the front end and bakc end code added to allow logged in users to add new items
    extra details - if you click on a exisiting lisiting code for the front end is implemented taht shows the user extra details of it such as the comments bids who posted it ect but i need to implem,ement the back end for bidding and commenting.

---
+++ Application/Application/Data/Services/IListingService.cs
@@ -0,0 +1,11 @@
+﻿using Application.Models;
+
+namespace Application.Data.Services
+{
+    public interface IListingService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing> GetById(int id);
+    }
+}
